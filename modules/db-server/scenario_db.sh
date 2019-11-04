7a5ca0dc864988e00433256f43333291ac37e46f
Yaroslav Ryzhan <slavik.ryzhan@gmail.com>
2019-11-04 13:52:06 +0200

    fix problem

--- modules/db-server/scenario_db.sh
+++ modules/db-server/scenario_db.sh
@@ -4,15 +4,15 @@
 # yum provides '*/applydeltarpm'
 # yum install deltarpm -y
 # sudo yum install epel-release -y
-sudo su <<_EOF_
-useradd -m jenkins
-mkdir -p /home/jenkins/.ssh
-chmod 700 /home/jenkins/.ssh
-cat /tmp/id_rsa.pub >> /home/erkek/.ssh/authorized_keys
-cat /tmp/id_rsa.pub >> /home/jenkins/.ssh/authorized_keys
-chown jenkins:jenkins /home/jenkins/.ssh/ /home/jenkins/.ssh/authorized_keys
-usermod -a -G adm,video,google-sudoers jenkins
-_EOF_
+# sudo su <<_EOF_
+# useradd -m jenkins
+# mkdir -p /home/jenkins/.ssh
+# chmod 700 /home/jenkins/.ssh
+# cat /tmp/id_rsa.pub >> /home/erkek/.ssh/authorized_keys
+# cat /tmp/id_rsa.pub >> /home/jenkins/.ssh/authorized_keys
+# chown jenkins:jenkins /home/jenkins/.ssh/ /home/jenkins/.ssh/authorized_keys
+# usermod -a -G adm,video,google-sudoers jenkins
+# _EOF_
 
 # sudo mkdir -p /opt/catalogue-db/data
 # sudo mkdir -p /opt/user-db/scripts
