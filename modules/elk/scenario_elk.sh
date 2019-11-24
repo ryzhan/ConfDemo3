84f622163f94a46056609be1fd20147626bfedda
Yaroslav Ryzhan <slavik.ryzhan@gmail.com>
2019-11-24 13:33:23 +0200

    fix

--- modules/elk/scenario_elk.sh
+++ modules/elk/scenario_elk.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 sudo setenforce Permissive
 sudo sh -c 'echo "vm.max_map_count = 262144" >> /etc/sysctl.conf'
+
+sudo sh -c 'cat << EOF >> /etc/security/limits.conf
+*   soft    nproc   65000
+*   hard    nproc   1000000
+*   -    nofile  1048576
+root - memlock unlimited
+EOF'
+
 sudo sysctl -p
+
+
 echo "All Done"
 
 
